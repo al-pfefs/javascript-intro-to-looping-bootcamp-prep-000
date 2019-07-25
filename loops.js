769f0273720388f5d57fba64d2fe2ddac9256c5c
Ali Pfeffer <alpfefs@gmail.com>
2019-07-25 16:07:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,6 +23,9 @@ function doWhileLoop(num) {
   function incrementVariable() {
   i = i + 1;
   return i;
-  console.log('I run once regardless.')
+}
+do {
+    console.log('I run once regardless.')
+    while (incrementVariable() < num)
 }
 }
